a225d8e66524de5033b322fa1a0c330a0b63804d
mai <maicmi@me.com>
2022-02-15 16:50:24 +0700

    Home Link update

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ const Layout = ({ location, title, children }) => {
   if (isRootPath) {
     header = (
       <h1 className="main-heading">
-        <Link to="/">{title}</Link>
+        <Link to="https://openmymai.github.io">{title}</Link>
       </h1>
     )
   } else {
