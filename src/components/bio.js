aa95dc501b2081c2956ee42e6b10da0211ea1bef
mai <maicmi@me.com>
2022-02-15 09:06:43 +0700

    Clearn Post

--- src/components/bio.js
+++ src/components/bio.js
@@ -44,10 +44,10 @@ const Bio = () => {
       />
       {author?.name && (
         <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
+          เขียนโดย <strong>{author.name}</strong> {author?.summary || null}
           {` `}
           <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
+            คุณสามารถติดตาม twitter ของผมได้
           </a>
         </p>
       )}
